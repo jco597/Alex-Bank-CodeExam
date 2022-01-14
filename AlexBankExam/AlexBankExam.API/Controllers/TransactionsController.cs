77ffa400620c2fc2fb40e4c2580eb0fdafb137aa
Jefferson Ominga <jco597@outlook.com>
2022-01-14 21:41:32 +0800

    Added Solution for the API and Client in preparation for the code exam.

---
+++ AlexBankExam/AlexBankExam.API/Controllers/TransactionsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace AlexBankExam.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        
+    }
+}
